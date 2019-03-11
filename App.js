62960a150ffc6103c188d9c2b979df2b6a2fb1d4
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-03-11 17:10:57 +0200

    refactor: navigator solve some compile error, add some imports

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View, Platform } from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers',
-import { TabNavigator } from 'react-navigation',
-import { DeckListView, NewDeck } from 'components/'
+import reducer from './reducers'
+import History from './components/History'
+import { TabNavigator } from 'react-navigation'
+import { DeckListView } from './components/DeckListView'
+import { NewDeck } from './components/NewDeck'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
 
 function FlashcardsStatusBar ({backgroundColor, ...props}) {
     return (
@@ -15,39 +20,39 @@ function FlashcardsStatusBar ({backgroundColor, ...props}) {
 }
 
 const Tabs = TabNavigator({
-    DeckListView: {
-        screen: DeckListView,
-        navigationOptions: {
-            tabBarLabel: 'Decks',
-            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-        },
-    },
-    NewDeck: {
-        screen: NewDeck,
-        navigationOptions: {
-            tabBarLabel: 'Add Deck',
-            tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-        },
-    },
-}, {
-    navigationOptions: {
-        header: null
-    },
-    tabBarOptions: {
-        activeTintColor: Platform.OS === 'ios' ? purple : white,
-        style: {
-            height: 56,
-            backgroundColor: Platform.OS === 'ios' ? white : purple,
-            shadowColor: 'rgba(0, 0, 0, 0.24)',
-            shadowOffset: {
-                width: 0,
-                height: 3
+        DeckListView: {
+            screen: DeckListView,
+            navigationOptions: {
+                tabBarLabel: 'Decks',
+                tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
             },
-            shadowRadius: 6,
-            shadowOpacity: 1
+        },
+        NewDeck: {
+            screen: NewDeck,
+            navigationOptions: {
+                tabBarLabel: 'Add Deck',
+                tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+            },
+        },
+    }, {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: Platform.OS === 'ios' ? purple : white,
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? white : purple,
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            }
         }
-    }
-})
+});
 
 export default class App extends React.Component {
     render() {
--- components/NewDeck.js
+++ components/NewDeck.js
@@ -14,6 +14,6 @@ function mapStateToProps (state) {
     }
 }
 
-export  default connect(
+export default connect(
     mapStateToProps
 )(NewDeck)
